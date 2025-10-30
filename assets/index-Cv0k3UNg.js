526ea1ae741b93914a0e6f3fbd747a5e0229e093
alkaptin2030-jpg <alkaptin2030@gmail.com>
2025-10-30 14:38:13 +0300

    update code

--- assets/index-Cv0k3UNg.js
+++
@@ -1,6 +0,0 @@
-import{c as r,r as t}from"./index-Ds1sgGeR.js";/**
- * @license lucide-react v0.462.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const a=r("Check",[["path",{d:"M20 6 9 17l-5-5",key:"1gmf2c"}]]);var c=t.createContext(void 0);function i(e){const o=t.useContext(c);return e||o||"ltr"}export{a as C,i as u};
