948e0ce99944caf4b42832fc28cffb3b0b4f83d2
alkaptin2030-jpg <alkaptin2030@gmail.com>
2025-11-18 05:09:24 +0200

    update code

---
+++ assets/web-BM9ednu6.js
@@ -0,0 +1,1 @@
+import{$ as n}from"./index-BaJ4CYlQ.js";class p extends n{async canOpenUrl(e){return{value:!0}}async openUrl(e){return window.open(e.url,"_blank"),{completed:!0}}}export{p as AppLauncherWeb};
